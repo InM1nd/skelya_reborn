5e3d726a438b0befad216ceda910359f1a2a388c
InM1nd <zabolotny1111@gmail.com>
2024-10-17 16:49:15 +0200

    My solution. Better late than never.

--- src/components/ui/custom/BigButton.tsx
+++ src/components/ui/custom/BigButton.tsx
@@ -15,16 +15,15 @@ export const BigButton: React.FC<BigButtonProps> = ({ text, onClick }) => {
     <div className="w-full flex items-center">
       <button 
         className="w-full flex justify-between bg-purple-main items-center text-black text-2xl font-bold text-left uppercase pl-4 hover:bg-blue sm:text-3xl md:pl-12 md:text-5xl xl:text-8xl group hover:stroke-change transition-colors duration-300"
-        onMouseEnter={() => setIsHovered(true)}
+        onMouseEnter={() => setIsHovered(true)} 
         onMouseLeave={() => setIsHovered(false)}
         onClick={onClick}
         >
         {text}
-        <img 
-          src={"/svg/Arrow.svg"} 
-          alt='' 
-          className={`transition-colors duration-300 ${isHovered ? 'hovered' : ''} h-[60px] md:h-[80px] xl:h-full`} 
-        />
+          <svg viewBox="0 0 207 194" fill="none" xmlns="http://www.w3.org/2000/svg" className='w-fit h-[70px] sm:h-[100px] md:h-[150px] xl:h-[200px]'>
+            <path d="M4 4H202.769V190H4.00006L4 4Z" fill="#040000" stroke="#6D61FF" stroke-width="8" className={`fill-current transition-colors ${isHovered ? 'stroke-[#95D4F8]' : 'stroke-[#6D61FF]'}`}/>
+            <path d="M155.216 50.4985L65.3227 130.184M65.3227 130.184L66.0685 63.2519M65.3227 130.184L132.069 130.928" stroke="#6D61FF" stroke-width="8.31776" className={`fill-current transition-colors ${isHovered ? 'stroke-[#95D4F8]' : 'stroke-[#6D61FF]'}`}/>
+          </svg>
       </button>
     </div>
   );
