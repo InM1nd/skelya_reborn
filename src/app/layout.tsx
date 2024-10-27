950baebfe0a3a176fca6a69d8493afcd7db365df
InM1nd <zabolotny1111@gmail.com>
2024-10-27 12:04:51 +0100

    My solution. Better late than never.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Raleway, Unbounded } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { GoogleAnalytics  } from '@next/third-parties/google'
+import Script from "next/script";
 
 const raleway = Raleway({
   subsets: ["cyrillic", "latin"],
@@ -22,7 +23,14 @@ export const metadata: Metadata = {
   title: "Skelya Careers",
   description: "Skelya Careers",
   metadataBase: new URL('http://skelya.careers'),
-  keywords:[''],
+  keywords:[
+    'Кар&apos;єрна консультація', 
+    'Пошук роботи', 
+    'Профорієнтація', 
+    'Скеля', 
+    'Skelya',
+    '',
+  ],
 };
 
 export default function RootLayout({
@@ -32,13 +40,44 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      </head>
       <body
         className={`${raleway.variable} ${unbounded.variable} antialiased`}
       >
         {children}
         <GoogleAnalytics gaId="G-SLW6C9CCYM" />
         <Analytics/>
+
+        <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        >
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1254564902408772');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=PageView&noscript=1`}
+          />
+        </noscript>
       </body>
     </html>
   );
 }
+
+
+
