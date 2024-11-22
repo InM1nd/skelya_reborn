63221302417b0fc4323bf555a2280a000161703a
InM1nd <zabolotny1111@gmail.com>
2024-11-22 14:36:49 +0100

    My solution. Better late than never.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,7 +86,7 @@ export default function RootLayout({
           />
         </noscript>
         <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=YourIdGoesHere"
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MJPQTGFF"
             height="0" width="0" style={{ display: "none", visibility: "hidden" }}>
           </iframe>
         </noscript>
